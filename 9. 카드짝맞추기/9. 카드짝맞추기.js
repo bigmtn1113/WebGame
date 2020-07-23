4fe099332b675a9a424c94c77be48dc5cefaf4ec
20140339 <kva231@naver.com>
2020-07-23 22:04:10 +0900

    카드 뒷면 색 추가 게임 시작 시 카드 공개 및 외울시간 추가

--- 9. 카드짝맞추기/9. 카드짝맞추기.js
+++ 9. 카드짝맞추기/9. 카드짝맞추기.js
@@ -1,27 +1,48 @@
 var width = 4;
 var height = 3;
+var cardBackColorList = ['red', 'red', 'orange', 'orange', 'violet', 'violet', 'green', 'green', 'navy', 'navy', 'indigo', 'indigo'];
+var cardBackColor = [];
+
+while (cardBackColorList.length)
+	cardBackColor.push(cardBackColorList.splice(Math.floor(Math.random() * cardBackColorList.length), 1));
 
 function setCard(width, height) {
+	var clickFlag = false;
 	for (var i = 0; i < width * height; ++i) {
-		card = document.createElement('div');
+		var card = document.createElement('div');
 		card.className = 'card';
-		cardInner = document.createElement('div');
+		var cardInner = document.createElement('div');
 		cardInner.className = 'card-inner';
-		cardFront = document.createElement('div');
+		var cardFront = document.createElement('div');
 		cardFront.className = 'card-front';
-		cardBack = document.createElement('div');
+		var cardBack = document.createElement('div');
 		cardBack.className = 'card-back';
+		cardBack.style.backgroundColor = cardBackColor[i];
 		
 		cardInner.append(cardFront);
 		cardInner.append(cardBack);
 		card.append(cardInner);
 		(function(c) {
 			card.addEventListener('click', function() {
-				c.classList.toggle('flipped');
+				if (clickFlag)
+					c.classList.toggle('flipped');
 			});
 		})(card);
 		document.body.append(card);
 	}
+	
+	document.querySelectorAll('.card').forEach(function(card, index) {
+		setTimeout(function() {
+			card.classList.add('flipped');
+		}, 1000 + 100 * index);
+	});
+	
+	document.querySelectorAll('.card').forEach(function(card, index) {
+		setTimeout(function() {
+			card.classList.remove('flipped');
+			clickFlag = true;
+		}, 5200);
+	});
 }
 
 setCard(width, height);
