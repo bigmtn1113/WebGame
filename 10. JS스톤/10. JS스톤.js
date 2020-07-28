9ac74cec4434695ca9c7d82510124d7717a2391e
20140339 <kva231@naver.com>
2020-07-28 22:31:43 +0900

    턴 방식 기능, 덱에서 카드를 뽑은 후 필드에 배치하는 기능 구현

--- 10. JS스톤/10. JS스톤.js
+++ 10. JS스톤/10. JS스톤.js
@@ -1,14 +1,23 @@
-var rivalHero = document.querySelector('#rival-hero');
 var rivalDeck = document.querySelector('#rival-deck');
-var myHero = document.querySelector('#my-hero');
 var myDeck = document.querySelector('#my-deck');
+var rivalHero = document.querySelector('#rival-hero');
+var myHero = document.querySelector('#my-hero');
+var rivalField = document.querySelector('#rival-field');
+var myField = document.querySelector('#my-field');
+var rivalCost = document.querySelector('#rival-cost');
+var myCost = document.querySelector('#my-cost');
 
 var rivalDeckData = [];
 var myDeckData = [];
 var rivalHeroData;
 var myHeroData;
+var rivalFieldData = [];
+var myFieldData = [];
+var rivalCostData = 10;
+var myCostData = 10;
+var isMyTurn = true;
 
-function Card(isHero) {
+function Card(isHero, isMyCard) {
 	if (isHero) {
 		this.att = Math.ceil(Math.random() * 2);
 		this.hp = Math.ceil(Math.random() * 5) + 25;
@@ -17,8 +26,11 @@ function Card(isHero) {
 		this.hp = Math.ceil(Math.random() * 5);
 		this.cost = Math.floor((this.att + this.hp) / 2);
 	}
+	
+	if (isMyCard)
+		this.mine = true;
 }
-function cardFactory(isHero) { return new Card(isHero); }
+function cardFactory(isHero, isMyCard) { return new Card(isHero, isMyCard); }
 
 function connectDataAndDom(data, dom, isHero) {
 	var card = document.querySelector('.card-hidden .card').cloneNode(true);
@@ -32,27 +44,53 @@ function connectDataAndDom(data, dom, isHero) {
 		card.querySelector('.card-name').textContent = '영웅';
 	}
 	
+	card.addEventListener('click', function() {
+		if (isMyTurn) {
+			if (!data.mine) return;
+			if (myCostData - data.cost < 0) return;
+			
+			myField.append(card);
+			myFieldData.push(data);
+			myDeckData.splice(myDeckData.indexOf(data), 1);
+			myCostData -= data.cost;
+			myCost.textContent = myCostData;
+			
+			isMyTurn = false;
+		} else {
+			if (data.mine) return;
+			if (rivalCostData - data.cost < 0) return;
+			
+			rivalField.append(card);
+			rivalFieldData.push(data);
+			rivalDeckData.splice(rivalDeckData.indexOf(data), 1);
+			rivalCostData -= data.cost;
+			rivalCost.textContent = rivalCostData;
+			
+			isMyTurn = true;
+		}
+	});
+	
 	dom.append(card);
 }
 
 function createRivalDeck(cnt) {
 	for (var i = 0; i < cnt; ++i)
-		rivalDeckData.push(cardFactory());
+		rivalDeckData.push(cardFactory(false, false));
 	
 	rivalDeckData.forEach(function(data) { connectDataAndDom(data, rivalDeck); });
 }
 function createMyDeck(cnt) {
 	for (var i = 0; i < cnt; ++i)
-		myDeckData.push(cardFactory());
+		myDeckData.push(cardFactory(false, true));
 
 	myDeckData.forEach(function(data) { connectDataAndDom(data, myDeck); });
 }
 function createRivalHero() {
-	rivalHeroData = cardFactory(true);
+	rivalHeroData = cardFactory(true, false);
 	connectDataAndDom(rivalHeroData, rivalHero, true);
 }
 function createMyHero() {
-	myHeroData = cardFactory(true);
+	myHeroData = cardFactory(true, true);
 	connectDataAndDom(myHeroData, myHero, true);
 }
 
