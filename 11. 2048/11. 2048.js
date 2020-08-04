5eb49a51bd26283354bd4d0973a0fe2f8d3dbe63
20140339 <kva231@naver.com>
2020-08-04 22:05:40 +0900

    숫자 합칠때 아래 방향과 오른쪽 방향에서의 버그 해결

--- 11. 2048/11. 2048.js
+++ 11. 2048/11. 2048.js
@@ -77,14 +77,24 @@ window.addEventListener('keydown', function(e) {
 			
 			tableData.forEach(function(rowData, x) {
 				rowData.forEach(function(columnData, y) {
-					if (columnData) {
-						if (newData[y][newData[y].length - 1] === columnData)
-							newData[y][newData[y].length - 1] *= 2;
-						else
-							newData[y].unshift(columnData);
-					}
+					if (columnData)
+						newData[y].unshift(columnData);
 				});
 			});
+			for (var x = 0; x < 4; ++x) {
+				if (newData[x].length > 1 && newData[x][0] === newData[x][1]) {
+					newData[x][0] *= 2;
+					newData[x].splice(1, 1);
+				}
+				if (newData[x].length > 2 && newData[x][1] === newData[x][2]) {
+					newData[x][1] *= 2;
+					newData[x].splice(2, 1);
+				}
+				if (newData[x].length > 3 && newData[x][2] === newData[x][3]) {
+					newData[x][2] *= 2;
+					newData[x].splice(3, 1);
+				}
+			}
 			
 			[1, 2, 3, 4].forEach(function(rowData, x) {
 				[1, 2, 3, 4].forEach(function(columnData, y) {
@@ -99,7 +109,7 @@ window.addEventListener('keydown', function(e) {
 				rowData.forEach(function(columnData, y) {
 					if (columnData) {
 						if (newData[x][newData[x].length - 1] === columnData)
-							newData[x][newData[x].length - 1] *= 2;	
+							newData[x][newData[x].length - 1] *= 2;
 						else
 							newData[x].push(columnData);
 					}
@@ -117,13 +127,21 @@ window.addEventListener('keydown', function(e) {
 			
 			tableData.forEach(function(rowData, x) {
 				rowData.forEach(function(columnData, y) {
-					if (columnData) {
-						if (newData[x][newData[x].length - 1] === columnData)
-							newData[x][newData[x].length - 1] *= 2;	
-						else
-							newData[x].unshift(columnData);
-					}
+					if (columnData)
+						newData[x].unshift(columnData);
 				});
+				if (newData[x].length > 1 && newData[x][0] === newData[x][1]) {
+					newData[x][0] *= 2;
+					newData[x].splice(1, 1);
+				}
+				if (newData[x].length > 2 && newData[x][1] === newData[x][2]) {
+					newData[x][1] *= 2;
+					newData[x].splice(2, 1);
+				}
+				if (newData[x].length > 3 && newData[x][2] === newData[x][3]) {
+					newData[x][2] *= 2;
+					newData[x].splice(3, 1);
+				}
 			});
 			
 			[1, 2, 3, 4].forEach(function(rowData, x) {
